af0ecbc5db5c71f471dfc3b9d87d57711ebd1646
Mackan <thormax5@gmail.com>
2016-04-08 11:38:37 +0200

    Fix again

--- cmds/defaults.js
+++ cmds/defaults.js
@@ -1,7 +1,7 @@
 var defaults = {
 	"info": {
 		process: function(args, message, bot, settings){
-			var owner = "<@"+settings["owner"]+">";
+			var owner = bot.users.get("id", settings["owner"]).name;
 			bot.sendMessage(message.channel, "```\nMackBot Version "+settings["version"]+"\nPrefix: "+settings["prefix"]+"\nUsing: discord.js\nOwner: "+owner+"```");
 		},
 		"desc": "Bot info",
