3514e166f1a3af2df3f040673e0e896039d197e7
Mackan <thormax5@gmail.com>
2016-04-08 11:12:08 +0200

    Fix

--- cmds/defaults.js
+++ cmds/defaults.js
@@ -1,7 +1,8 @@
 var defaults = {
 	"info": {
 		process: function(args, message, bot, settings){
-			bot.sendMessage("```\nMackBot Version "+settings["version"]+"\nPrefix: "+settings["prefix"]+"\nUsing: discord.js\nOwner: <@"+settings["qwner"]+">```");
+			var owner = "<@"+settings["owner"]+">";
+			bot.sendMessage(message.channel, "```\nMackBot Version "+settings["version"]+"\nPrefix: "+settings["prefix"]+"\nUsing: discord.js\nOwner: "+owner+"```");
 		},
 		"desc": "Bot info",
 		"usage": "info"
