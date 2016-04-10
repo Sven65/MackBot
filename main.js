133e719e04cbb51e92d40a534ef83dfafae7b2cc
Mackan <thormax5@gmail.com>
2016-04-10 11:53:47 +0200

    Update

--- main.js
+++ main.js
@@ -6,12 +6,27 @@ var chalk = require('chalk');
 var mybot = new Discord.Client();
 
 var settings = require("./settings.json");
+var toggled = require("./data/toggled.json");
 
 var misc = require("./cmds/misc.js").misc;
 var admin = require("./cmds/admin.js").admin;
 var util = require("./cmds/util.js").util;
 var nsfw = require("./cmds/nsfw.js").nsfw;
 var defaults = require("./cmds/defaults.js").defaults;
+var hardCoded = {
+	"help": {
+		"desc": "Shows help",
+		"usage": "help `[command]`"
+	},
+	"nsfw": {
+		"desc": "Toggles NSFW commands for channel",
+		"usage": "nsfw"
+	},
+	"commands": {
+		"desc": "Shows commands",
+		"usage": "commands"
+	}
+}
 
 var commands = extend({}, misc, admin, util, nsfw, defaults);
 
@@ -27,11 +42,31 @@ var processed = 0;
 
 // Misc functions
 
+function saveToggle(){
+	fs.writeFile("./data/toggled.json", JSON.stringify(toggled), 'utf8', function(err){
+		if(err){ throw err; }
+	});
+}
+
+function checkRole(message, role){
+	var roles = message.channel.server.rolesOfUser(message.author);
+	for(i=0;i<roles.length;i++){
+		if(roles[i].name == role){
+			return true;
+		}
+	}
+	return false;
+}
+
 function init(){
 	var now = new Date().valueOf();
 	for(i=0;i<Object.keys(commands).length;i++){
 		firstTime[Object.keys(commands)[i]] = {};
+		if(!toggled.hasOwnProperty(Object.keys(commands)[i])){
+			toggled[Object.keys(commands)[i]] = [];
+		}
 	}
+	saveToggle();
 	loadNSFWChans();
 
 	if(settings['bot']['token'].length > 0){
@@ -103,9 +138,10 @@ mybot.on("message", function(message){
 				mybot.sendMessage(message.channel, "I'm MackBot! Type "+settings['prefix']['main']+"commands for a list of commands!");
 			}
 		}else if(args[0] == settings['prefix']['main']+"commands" || settings['prefix']['botname'] && args[0] == "<@"+mybot.user.id+">" && args[1] == "commands"){
+			var dflt = extend({}, defaults, hardCoded);
 			var helpMsg = "__**Commands:**__\n\n";
 			helpMsg += "**Defaults: **";
-			helpMsg += Object.keys(defaults).sort().join(", ");
+			helpMsg += Object.keys(dflt).sort().join(", ");
 		    helpMsg += "\n\n**Misc: **";
 		    helpMsg += Object.keys(misc).sort().join(", ")
 		    helpMsg += "\n\n**Admin: **";
@@ -118,7 +154,7 @@ mybot.on("message", function(message){
 		    }
 		    mybot.sendMessage(message.channel, helpMsg);
 		}else if(args[0] == settings['prefix']['main']+"nsfw" || settings['prefix']['botname'] && args[0] == "<@"+mybot.user.id+">" && args[1] == "nsfw"){
-			if(settings["admins"].indexOf(message.author.id) > -1){
+			if(settings["admins"].indexOf(message.author.id) > -1 || checkRole(message, settings['adminrole'])){
 				var chan = message.channel.id;
 				if(nsfwChans.indexOf(chan) > -1){
 					nsfwChans.splice(nsfwChans.indexOf(chan), 1);
@@ -134,6 +170,27 @@ mybot.on("message", function(message){
 					});
 				}
 			}
+		}else if(args[0] == settings['prefix']['main']+"toggle" || settings['prefix']['botname'] && args[0] == "<@"+mybot.user.id+">" && args[1] == "toggle"){
+			if(settings["admins"].indexOf(message.author.id) > -1 || checkRole(message, settings['adminrole'])){
+				if(args.length == 2){
+					cmd = args[1];
+					if(Object.keys(commands).indexOf(cmd) > -1){
+						var tgl = toggled[cmd];
+						var id = message.channel.server.id;
+						var index = tgl.indexOf(id);
+
+						if(index > -1){
+							tgl.splice(index, 1);
+							mybot.sendMessage(message.channel, "Command "+cmd+" enabled for server.");
+						}else{
+							toggled[cmd].push(id);
+							mybot.sendMessage(message.channel, "Command "+cmd+" disabled for server.");
+						}
+
+						saveToggle();
+					}
+				}
+			}
 		}else{
 			if(args[0].substring(0, settings['prefix']['main'].length) == settings['prefix']['main'] || settings['prefix']['botname'] && args[0] == "<@"+mybot.user.id+">"){
 				var cmd;
@@ -145,7 +202,9 @@ mybot.on("message", function(message){
 				}
 
 				var index = Object.keys(commands).indexOf(cmd);
-				if(index > -1){
+				var disabled = toggled[cmd].indexOf(message.channel.server.id);
+
+				if(index > -1 && disabled == -1){
 					if(!commands[cmd].nsfw){
 						var now = new Date().valueOf();
 						if(!lastExecTime.hasOwnProperty(cmd)){
@@ -156,7 +215,7 @@ mybot.on("message", function(message){
 							lastExecTime[cmd][message.author.id] = new Date().valueOf();
 						}
 
-						if(settings["admins"].indexOf(message.author.id) == -1){
+						if(settings["admins"].indexOf(message.author.id) == -1 || !checkRole(message, settings['adminrole'])){
 							if(now < lastExecTime[cmd][message.author.id] + (commands[cmd].cooldown * 1000) && firstTime[cmd].hasOwnProperty(message.author.id)){
 								mybot.sendMessage(message, message.author.username.replace(/@/g, '@\u200b') + ", you need to *cooldown* (" + Math.round(((lastExecTime[cmd][message.author.id] + commands[cmd].cooldown * 1000) - now) / 1000) + " seconds)", function(e, m){ mybot.deleteMessage(m, {"wait": 6000}); });
 								if (!message.channel.isPrivate) mybot.deleteMessage(message, {"wait": 10000});
@@ -181,7 +240,7 @@ mybot.on("message", function(message){
 								lastExecTime[cmd][message.author.id] = new Date().valueOf();
 							}
 
-							if(settings["admins"].indexOf(message.author.id) == -1){
+							if(settings["admins"].indexOf(message.author.id) == -1 || !checkRole(message, settings['adminrole'])){
 								if(now < lastExecTime[cmd][message.author.id] + (commands[cmd].cooldown * 1000)){
 									mybot.sendMessage(message, message.author.username.replace(/@/g, '@\u200b') + ", you need to *cooldown* (" + Math.round(((lastExecTime[cmd][message.author.id] + commands[cmd].cooldown * 1000) - now) / 1000) + " seconds)", function(e, m){ mybot.deleteMessage(m, {"wait": 6000}); });
 									if(!message.channel.isPrivate) mybot.deleteMessage(message, {"wait": 10000});
